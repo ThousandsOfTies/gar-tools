c649e38a512d5b4e88e40606c134ebc392f31616
thousands.of.ties <thousands.of.ties@gmail.com>
2026-07-07 21:58:47 +0900

    Add color565 helper method to M5Unified

--- targets/esp32/wokwi/m5stackc/lib/M5Unified/src/M5Unified.h
+++ targets/esp32/wokwi/m5stackc/lib/M5Unified/src/M5Unified.h
@@ -46,6 +46,9 @@ class DisplayShim {
 
   int16_t width() const { return LOGICAL_WIDTH; }
   int16_t height() const { return LOGICAL_HEIGHT; }
+  uint16_t color565(uint8_t red, uint8_t green, uint8_t blue) const {
+    return ((red & 0xF8) << 8) | ((green & 0xFC) << 3) | (blue >> 3);
+  }
 
   void setBrightness(uint8_t brightness) { brightness_ = brightness; }
   void fillScreen(uint16_t color) {
